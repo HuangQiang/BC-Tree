8bb29345062bebe5ae41f66074b489858798c647
Huang Qiang <huangq2011@gmail.com>
2022-11-26 21:23:05 +0800

    polish codes

--- methods/def.h
+++ methods/def.h
@@ -43,12 +43,11 @@ const int RANDOM_SEED        = 6;
 const int M                  = 100;   // statistics
 const int SCAN_SIZE          = 64;    // RQALSH and QALSH
 const int N_PTS              = 100;   // threshold for indexing (FH)
-const f32 LEAF_RATIO         = 0.005; // ratio     for indexing (metric-tree)
-const int MIN_LEAF_PTS       = 1000;  // threshold for indexing (metric-tree)
-const int MAX_BLOCK_NUM      = 1000000; // FH (25,000 for 1,000,000; 1,000,000 for 100,000,000) 
+
+const int MAX_BLOCK_NUM      = 25000; // set 1000000 for Sift100M and Deep100M 
 const std::vector<f32> Cs    = { 10.0f };
 const std::vector<int> Ls    = { 1,2,3 };
-const std::vector<int> TOPKs = { 1, 10, }; // { 1,10,20,40 }; { 1,10,20,40,60,80,100 };
+const std::vector<int> TOPKs = { 1,10,20,40 }; // { 1,10,20,40,60,80,100 };
 
 const int MAXK               = 100; // TOPKs.back(); 
 const int THREAD_NUM         = 50;
